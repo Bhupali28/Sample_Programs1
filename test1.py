eebf87f57bf9fbfbe7f749e53ab15f7213382dc8
Bhupali28 <bhupali.patil@opcito.com>
2017-05-08 18:11:15 +0530

    These are sample programs

---
+++ test1.py
@@ -0,0 +1,20 @@
+list = [1 , 2 , "Hello" , "World", 2017]     #list examples
+
+print list[3], "\n" , list[1:4]
+list[1] = 3;
+print list
+del list[1];
+print list
+
+
+
+tup = (1,2,3,4,5);
+tup1 = (6,);
+print tup[2]
+
+if (tup > tup1):
+	print ("tup is greater than tup1")
+else:
+	print ("tup1 is greater than tup")
+
+
