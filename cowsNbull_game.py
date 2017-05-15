7c57a6774da466a2dacf9b5d2b4dd963348b904a
Bhupali28 <bhupali.patil@opcito.com>
2017-05-15 14:52:48 +0530

    updated program

--- cowsNbull_game.py
+++ cowsNbull_game.py
@@ -4,37 +4,31 @@ import random
 
 randNum = '0123456789'
 
-randNum = random.sample(randNum,4)
+randNum = list(random.sample(randNum,4))
 
-randnum = int (''.join (random.sample(randNum,4)))
-print randnum
+print randNum
 cows = 0
 bulls = 0
 while True:
 	print ("Welcome to the Cows and Bulls game")
-	num = raw_input("Enter the 4 digit number ")	
-
-		
+	num = raw_input("Enter the 4 digit number ")
+	
 	if num == 'exit':
 		break
 		
-	num = int(num)
-
+	num = list (num)
+	print num
 	for i in range(0,4):
-		
-		no = num % 10
+		if num[i] == randNum[i]:
+			cows +=1
+		elif num[i] in randNum:
+			bulls +=1
+	print 'cows : ',cows
+	print 'bulls : ',bulls
 	
-		r_no = randnum % 10
-
-		if (no == r_no):
-			cows += 1
-		elif str(no) in randNum:	
-			bulls += 1	
-		num = num/10
-			
-		randnum = randnum/10
-		
-	print cows,bulls
+	if num == randNum :
+		print("Congrates ! You have guessed correct number...")
+		break
 
 	
-	
+
